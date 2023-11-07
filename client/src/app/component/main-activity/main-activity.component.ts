1280c46662cfada5c9cc245f18f3b09dd023960d
Sumant-Dusane <sumant.dusane1966@gmail.com>
2023-11-07 18:19:13 +0530

    code clean and commenting on live features

--- client/src/app/component/main-activity/main-activity.component.ts
+++ client/src/app/component/main-activity/main-activity.component.ts
@@ -9,6 +9,8 @@ import { DrawService } from 'src/app/services/DrawService/draw.service';
   styleUrls: ['./main-activity.component.scss']
 })
 export class MainActivityComponent implements AfterViewInit {
+
+  // Decelaring/Initializing Variables
   @ViewChild('drawBoard', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
   context!: CanvasRenderingContext2D | null;
   isDrawing: boolean = false;
@@ -27,8 +29,10 @@ export class MainActivityComponent implements AfterViewInit {
     y: 0,
   };
 
+  // Instantiate service file classes
   constructor(private socket: SocketService, private drawingService: DrawService) { }
 
+  // Initializing Canvas as soon as Angular Lifecycle fully initialized component's view
   ngAfterViewInit(): void {
     this.canvas.nativeElement.width = (window.innerWidth * 80) / 100;      // 90% of the screen
     this.canvas.nativeElement.height = (window.innerHeight * 85) / 100;   // 80% of the screen
@@ -51,6 +55,7 @@ export class MainActivityComponent implements AfterViewInit {
     })
   }
 
+  // Instantiate canvas context and assign snapshot
   initCanvas() {
     this.context = this.canvas.nativeElement?.getContext("2d");
     this.context!.fillStyle = this.drawingAttributes.bgcolor;
@@ -58,7 +63,7 @@ export class MainActivityComponent implements AfterViewInit {
     this.snapshot = this.context!.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
-
+  // Triggers when another client tries to trigger mousedown listener
   onDown(drawingAttributes: any) {
     this.context?.beginPath();
 
@@ -69,6 +74,7 @@ export class MainActivityComponent implements AfterViewInit {
     this.context!.lineCap = drawingAttributes.lineCap;
   }
 
+  // Triggers when another client is drawing on his board
   onMove(data: any) {
     const { shapeDrawableVariables, drawingAttributes } = data;
     switch (this.drawingAttributes.shape) {
@@ -90,10 +96,12 @@ export class MainActivityComponent implements AfterViewInit {
     }
   }
 
+  // Triggers when another client ends up drawing
   onUp(x: any, y: any) {
     this.context?.moveTo(x, y);
   }
 
+  // Triggers when self device notices mousedown listener
   mouseDownListener(e: any) {
     this.isDrawing = true;
     this.context?.beginPath();
@@ -107,11 +115,12 @@ export class MainActivityComponent implements AfterViewInit {
     this.context!.lineJoin = this.drawingAttributes.lineJoin;
     this.context!.lineCap = this.drawingAttributes.lineCap;
 
-    this.socket.initBoard(this.drawingAttributes);
+    this.socket.initBoard(this.drawingAttributes);            // call Service file to bind the user mousedown to all connected clients
 
     // this.snapshot = this.context?.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
+  // Triggers when self device notices mousemove listener
   mouseMoveListener(e: any) {
     var shapeDrawableVariables;
     if (!this.isDrawing) return
@@ -126,7 +135,7 @@ export class MainActivityComponent implements AfterViewInit {
           shapeDrawableVariables,
           drawingAttributes: this.drawingAttributes
         }
-        this.socket.bindCanvas(data);
+        this.socket.bindCanvas(data);                             // call Service file to bind the user mousemove to all connected clients
         this.drawingService.drawFreeForm(shapeDrawableVariables, this.drawingAttributes, this.context);
         break;
       case 'circle':
@@ -165,31 +174,35 @@ export class MainActivityComponent implements AfterViewInit {
     }
   }
 
+  // Triggers when self device notices mouseup listener
   mouseUpListener(e: any) {
     let data = {
       x: e.clientX - this.canvas.nativeElement.offsetLeft,
       y: e.clientY - this.canvas.nativeElement.offsetTop
     }
-    this.socket.stopDrawing(data);
+    this.socket.stopDrawing(data);              // call Service file to bind the user mouseup to all connected clients
     this.isDrawing = false;
     this.context?.stroke();
     this.context?.closePath();
   }
 
+  // Get emitted value from sidebar (child) component and upates accordingly
   updateDrawingAttributes(drawingAttributes: DrawingAttrs) {
     this.drawingAttributes = drawingAttributes;
   }
 
+  // Get emitted value from sidebar (child) component and clear the board
   clearBoard(isServer: boolean = false) {
     this.context?.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     this.initCanvas();
-    if (!isServer) this.socket.clearCanvas();
+    if (!isServer) this.socket.clearCanvas();               // call Service file to clear the board from all clients
   }
 
-
+  // Get emitted value from header (child) component and generated download link
   downloadLink() {
     const url = this.canvas.nativeElement.toDataURL('image/png');
     const anchor = document.createElement('a');
+    //  Functionality is it programatically creates anchor tag and triggest onclick
     anchor.href = url;
     anchor.download = 'image.png'
     anchor.click();
